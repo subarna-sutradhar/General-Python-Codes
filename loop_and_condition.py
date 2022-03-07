4ecc238c4fca5b02e97b27f9bb490b8b5aac6281
Subarna Sutradhar <subarna04work@gmail.com>
2022-03-07 10:53:40 +0530

    Create loop_and_condition.py

---
+++ loop_and_condition.py
@@ -0,0 +1,8 @@
+x=int(input("enter first number"))
+y=int(input("enter second number"))
+if x==y:
+    print("equal")
+elif x>y:
+    print("first number is greater")
+else:
+    print("second number is greater")
