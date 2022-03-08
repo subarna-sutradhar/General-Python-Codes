19726605d53aab75b109bafe4329bd4071e8cf4b
Subarna Sutradhar <subarna04work@gmail.com>
2022-03-08 18:32:33 +0530

    Create stacks.py

---
+++ stacks.py
@@ -0,0 +1,31 @@
+
+class stack():
+    def __init__(self):
+        self.items = []
+
+    def push(self, item):
+        self.items.append(item)
+    
+    def pop(self):
+        return self.items.pop()
+
+    def is_empty(self):
+        return self.items == []
+    
+    def peek(self):
+        if not self.is_empty():
+            return self.items[-1]
+
+    def get_stack(self):
+        return self.items
+
+s = stack()
+print(s.is_empty())
+s.push("A")
+s.push("B")
+s.push("C")
+#print(s.get_stack())
+#s.pop()
+#print(s.get_stack())
+s.push("D")
+print(s.peek())
