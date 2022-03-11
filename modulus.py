699b5f5f7f488cb6e0f5487cddaedfba543f4239
Subarna Sutradhar <subarna04work@gmail.com>
2022-03-11 19:13:24 +0530

    Create modulus.py

---
+++ modulus.py
@@ -0,0 +1,7 @@
+def him():
+    a=int(input("enter the first number\t"))
+    b=int(input("enter the second number\t"))
+    if(a%b<10):
+        print('ha ha ha')
+    else:
+        print('he he he')
