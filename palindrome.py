bd6e89a40a593d894450de013200a6c0a1f823f9
Subarna Sutradhar <subarna04work@gmail.com>
2022-03-11 19:12:41 +0530

    Create palindrome.py

---
+++ palindrome.py
@@ -0,0 +1,7 @@
+def palitrone():
+    str=input("enter a string")
+    if(str==str[::-1]):
+        print("its a palitrone")
+        #example of a palitrom is "CIVIC" , "MADAM" ,etc
+    else:
+        print("its not a palitrone")
