7dd301c5b52b5b08762eaf3d329ccf6d8e1a7b97
Subarna Sutradhar <subarna04work@gmail.com>
2022-03-13 10:38:00 +0530

    Create matrix_diagonalsfilled.py

---
+++ matrix_diagonalsfilled.py
@@ -0,0 +1,8 @@
+"""matrix with diagonals filled"""
+for i in range(5):
+    for j in range(5):
+        if i == j or i+j == 4:
+            print("$", end = " ")
+        else:
+            print("*", end = " ")
+    print()
