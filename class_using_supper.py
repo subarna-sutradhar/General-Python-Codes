599af5d658fe8a2c221a7fc841d1a5e45c16e82f
Subarna Sutradhar <subarna04work@gmail.com>
2022-03-15 16:35:50 +0530

    Create class_using_supper.py

---
+++ class_using_supper.py
@@ -0,0 +1,13 @@
+"""USING SUPER"""
+class Square:
+    def __init__(self,side):
+        self.side = side
+
+    def area(self):
+        return self.side * self.side
+
+class Cube(Square):
+    def area(self):
+        return super().area() * 6 """super calls the area of Square class"""
+    def volume(self):
+        return super().area() * self.side
