1cda5d3e0c60afead8e11afbc98783bfbb2eca6e
Subarna Sutradhar <subarna04work@gmail.com>
2022-03-21 18:42:59 +0530

    Create pandas04.py

---
+++ pandas04.py
@@ -0,0 +1,6 @@
+import pandas as pd
+data={'Name':['Subarna','Debanshu','Tuhi','Mohak','Ahana','Sayan','Tushar'],
+      'Age':[18,19,18,19,18,19,20],
+      'Ph_numbers':[100235,340254,254855,254136,457896,412451,356214]}
+df = pd.DataFrame(data)
+print(df)
